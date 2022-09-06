b4250dcab1e9d26cbeadf53ab4af6a0c5d7427ad
Brandon Truong <73107239+BrandonTruonggg@users.noreply.github.com>
2022-09-05 23:56:02 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///information.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
-def home():
-    return "Cryptocurrency Forecasting Model"
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    data_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/information')
+def information():
+    return render_template('information.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
