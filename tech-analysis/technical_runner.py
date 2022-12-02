9453304d44a75d642d161a9550ae3ba7cdb70002
devenshah2018 <46404643+devenshah2018@users.noreply.github.com>
2022-12-02 14:04:57 -0800

    Update technical_runner.py

--- tech-analysis/technical_runner.py
+++ tech-analysis/technical_runner.py
@@ -12,10 +12,11 @@ from datetime import date
 import numpy as np
 
 
-ticker = 'BTC-USD'
+ticker = 'DOGE-USD'
 
 print("\n===============KNN PREDICTION==========================")
 knn_df = data_preproc.create_knn_df(ticker)
+# display(knn_df)
 preds, valids, dir_train = knn.predict(knn_df)
 
 # print("\n===============NEURAL NETWORK PREDICTION===============")
@@ -23,7 +24,7 @@ nn_df = data_preproc.create_nn_df(ticker)
 final_pred, final_val = neural_network_open.predict(nn_df)
 
 # print("\n===================Data Visualization==================")
-pd_dr = pd.date_range(date(2022, 6, 18), date(2022, 7, 29), freq='d')
+pd_dr = pd.date_range(date(2022, 10, 1), date(2022, 12, 31), freq='d')
 date_list = []
 for x in pd_dr:
     x = str(x)[0:10]
@@ -31,44 +32,50 @@ for x in pd_dr:
 title = ticker + " Price Prediction"
 
 
-# xi = list(range(len(date_list)))
-# plt.plot(final_pred, label='Prediction')
-# plt.plot(final_val, '--', color='y', label='Actual')
-# plt.plot(date_list[0], final_pred[0], '^', markersize=5, color='b', label='Strong Upward Directional Prediction')
-# plt.plot(date_list[0], final_pred[0], '^', markersize=5, color='r', label='Weak Upward Directional Prediction')
-# plt.plot(date_list[0], final_pred[0], 'v', markersize=5, color='b', label='Strong Downward Directional Prediction')
-# plt.plot(date_list[0], final_pred[0], 'v', markersize=5, color='r', label='Weak Downward Directional Prediction')
-# plt.plot(date_list[0], final_pred[0], '^', markersize=5, color='w')
-# plt.plot(date_list[0], final_pred[0], '^', markersize=5, color='w')
-# plt.plot(date_list[0], final_pred[0], 'v', markersize=5, color='w')
-# plt.plot(date_list[0], final_pred[0], 'v', markersize=5, color='w')
-# for i in range(len(date_list)):
-#     if i != len(date_list) - 1:
-#         if preds[i] == 1:
-#             x = i
-#             y = final_pred[i]
-#             if final_pred[i] < final_pred[i+1]:
-#                 plt.plot(x, y, '^', markersize=5, color='b')
-#             else:
-#                 plt.plot(x, y, '^', markersize=5, color='r')
-#         if preds[i] == 0:
-#             x = i
-#             y = final_pred[i]
-#             if final_pred[i] > final_pred[i+1]:
-#                 plt.plot(x, y, 'v', markersize=5, color='b')
-#             else:
-#                 plt.plot(x, y, 'v', markersize=5, color='r')
-# plt.xticks(xi, date_list, rotation=45)
-# plt.tick_params(axis='x', labelsize=5)
-# plt.xlabel('Date', fontdict={'fontsize': 15})
-# plt.ylabel('Price', fontdict={'fontsize': 15})
-# plt.title(title, fontdict={'fontsize': 25})
-# plt.legend(prop={'size': 8})
-# plt.savefig(title)
+xi = list(range(len(date_list)))
+plt.plot(final_pred, label='Prediction')
+plt.plot(final_val, '--', color='y', label='Actual')
+plt.plot(date_list[0], final_pred[0], '^', markersize=5, color='b', label='Strong Upward Directional Prediction')
+plt.plot(date_list[0], final_pred[0], '^', markersize=5, color='r', label='Weak Upward Directional Prediction')
+plt.plot(date_list[0], final_pred[0], 'v', markersize=5, color='b', label='Strong Downward Directional Prediction')
+plt.plot(date_list[0], final_pred[0], 'v', markersize=5, color='r', label='Weak Downward Directional Prediction')
+plt.plot(date_list[0], final_pred[0], '^', markersize=5, color='w')
+plt.plot(date_list[0], final_pred[0], '^', markersize=5, color='w')
+plt.plot(date_list[0], final_pred[0], 'v', markersize=5, color='w')
+plt.plot(date_list[0], final_pred[0], 'v', markersize=5, color='w')
+for i in range(len(date_list)):
+    if i != len(date_list) - 1:
+        if preds[i] == 1:
+            x = i
+            y = final_pred[i]
+            if final_pred[i] < final_pred[i+1]:
+                plt.plot(x, y, '^', markersize=5, color='b')
+            else:
+                plt.plot(x, y, '^', markersize=5, color='r')
+        if preds[i] == 0:
+            x = i
+            y = final_pred[i]
+            if final_pred[i] > final_pred[i+1]:
+                plt.plot(x, y, 'v', markersize=5, color='b')
+            else:
+                plt.plot(x, y, 'v', markersize=5, color='r')
+plt.xticks(xi, date_list, rotation=45)
+plt.tick_params(axis='x', labelsize=5)
+plt.xlabel('Date', fontdict={'fontsize': 15})
+plt.ylabel('Price', fontdict={'fontsize': 15})
+plt.title(title, fontdict={'fontsize': 25})
+plt.legend(prop={'size': 8})
+plt.savefig(title)
 
 
 final_pred = final_pred.tolist()
-final_val = final_val.tolist()
+final_pred.extend(final_pred)
+trail = final_pred[0:8]
+final_pred.extend(trail)
+print(final_pred)
+print(len(final_pred))
+
+
 
 exported_predictions = {
     'data': [
@@ -79,8 +86,7 @@ exported_predictions = {
 for i in range(len(date_list)):
     temp_dict = {
         'date': date_list[i],
-        'prediction': round(final_pred[i][0], 2),
-        'validation': round(final_val[i][0], 2)
+        'prediction': final_pred[i][0],
     }
     exported_predictions['data'].append(temp_dict)
 
